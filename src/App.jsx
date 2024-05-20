e69ccaea6b692f9a5e4f80bc026449ac5542a0e6
Ezequiel Lino <sonisombra@gmail.com>
2024-05-19 20:52:15 -0600

    componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-
+import Header from "./components/Header"
 
 function App() {
 
-  const auth = false;
-
   return (
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
-      {auth ? 'autenticado' : 'no autenticado'}
+      <Header/>
     </div>
   )
 }
